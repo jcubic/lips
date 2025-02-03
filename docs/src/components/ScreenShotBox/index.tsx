52f3af5b6ceec528ef396aa0be52c94b31704790
Jakub Jankiewicz <jcubic@onet.pl>
2025-02-03 21:40:02 +0100

    (docs) add resizing to screenshooter #438

--- docs/src/components/ScreenShotBox/index.tsx
+++ docs/src/components/ScreenShotBox/index.tsx
@@ -1,3 +1,9 @@
+import React, {
+    useState,
+    useRef,
+    useEffect,
+    CSSProperties
+} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useScripts from '@site/src/hooks/useScripts';
 import Head from '@docusaurus/Head';
@@ -16,12 +22,50 @@ const example_code = `(define re #/<h1>([^>]+)<\\/h1>/)
     (loop (- i 1))))
 `;
 
+const WIDTH_MIN = 50;
+
 export default function ScreenShotBox(): JSX.Element {
+    const dragging = useRef<boolean>(false);
+    const dialogRef = useRef<HTMLDivElement>(null);
+    const boxRef = useRef<HTMLDivElement>(null);
+    const resizerRef = useRef<HTMLDivElement>(null);
+
     const { siteConfig } = useDocusaurusContext();
+
     useScripts([
         'https://cdn.jsdelivr.net/combine/npm/codemirror@5.58.3/lib/codemirror.js,npm/codemirror@5.58.3/mode/javascript/javascript.js,npm/codemirror@5.58.3/addon/edit/matchbrackets.js,gh/jcubic/lips@devel/lib/js/codemirror.js',
         `${siteConfig.baseUrl}js/screenshooter.js`
     ]);
+
+    useEffect(() => {
+        function mouseMove(event: MouseEvent) {
+            if (dragging.current) {
+                const screenX = event.screenX;
+                const rect = resizerRef.current.getBoundingClientRect();
+                let diff = screenX - rect.left;
+                let width = dialogRef.current.clientWidth + diff;
+                if (width < WIDTH_MIN) {
+                    width = WIDTH_MIN + 1;
+                }
+                boxRef.current.style.setProperty('--width', `${width}px`);
+            }
+        }
+        function mouseUp() {
+            dragging.current = false;
+        }
+        document.addEventListener('mousemove', mouseMove);
+        document.addEventListener('mouseup', mouseUp);
+        return () => {
+            document.removeEventListener('mousemove', mouseMove);
+            document.removeEventListener('mouseup', mouseUp);
+        }
+    }, []);
+
+
+    function dragStart() {
+        dragging.current = true;
+    }
+
     return (
         <>
           <Head>
@@ -30,18 +74,21 @@ export default function ScreenShotBox(): JSX.Element {
             <link href="https://cdn.jsdelivr.net/npm/codemirror@5.58.3/addon/hint/show-hint.css" rel="stylesheet"/>
             <script src="https://cdn.jsdelivr.net/npm/html-to-image@1.10.4/dist/html-to-image.js"></script>
           </Head>
-          <div className="box">
+          <div className="box" ref={boxRef}>
             <pre className="hidden">{example_code}</pre>
             <div className="wrapper">
-              <div className="cm-dialog">
-                <header>
-                  <ul className="cm-icons">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                </header>
-                <div className="cm-body"></div>
+              <div className="resizable">
+                <div className="cm-dialog" ref={dialogRef}>
+                  <header>
+                    <ul className="cm-icons">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </header>
+                  <div className="cm-body"></div>
+                </div>
+                <div className="resizer" ref={resizerRef} onMouseDown={dragStart}></div>
               </div>
             </div>
             <footer>
