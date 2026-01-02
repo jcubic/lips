530d48e9f18b7ab964234490504c41deb3d993fa
Jakub Jankiewicz <jcubic@onet.pl>
2026-01-02 15:49:59 +0100

    fix Unit tests in Node 24

--- tests/helpers/helpers.scm
+++ tests/helpers/helpers.scm
@@ -7,8 +7,8 @@
         (a_name (gensym))
         (b_name (gensym)))
     `(let ((,attempt (t.try (lambda (e)
-                              (let ((,a_name ,a)
-                                    (,b_name ,b))
+                              (let ((,a_name (round-number ,a))
+                                    (,b_name (round-number ,b)))
                                 (if (equal? ,a_name ,b_name)
                                     (--> e (pass))
                                     (--> e (fail (concat "failed: " (repr ,a_name true)
@@ -20,6 +20,36 @@
                            (set-obj! e 'savedError #void)))))
        (--> ,attempt (commit)))))
 
+(define (round-number x . rest)
+  "(round-number x)
+
+  Rounds float numbers to a given numbers of digits after decimal point.
+  This fixes an aissue with V8 in Node 24 which increased presition."
+  (let ((presition (if (null? rest) 10 (car rest))))
+    (if (number? x)
+        (cond ((string=? x.__type__ "float")
+               (round-fixed x presition))
+              ((and (string=? x.__type__ "complex")
+                    (string=? x.__im__.__type__ "float")
+                    (string=? x.__re__.__type__ "float"))
+               (make-rectangular (round-fixed x.__re__ presition)
+                                 (round-fixed x.__im__ presition)))
+              ((and (string=? x.__type__ "complex")
+                    (= x.__re__ 0)
+                    (string=? x.__im__.__type__ "float"))
+               (make-rectangular 0
+                                 (round-fixed x.__im__ presition)))
+              (else x))
+        x)))
+
+(define (round-fixed x . rest)
+  "(round-fixed x)
+
+   Rounds a number to a given presition"
+  (let* ((presition (if (null? rest) 14 (car rest)))
+         (factor (** 10 presition)))
+    (/ (Math.round (* x factor))
+       (exact->inexact factor))))
 
 (define-macro (to.throw . body)
   "(to.throw code)
