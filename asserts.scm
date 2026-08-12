b6c456730ddc03d8707ad4de1b4eb6413d076b48
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-08-12 14:34:04 +0200

    more tests

--- asserts.scm
+++ asserts.scm
@@ -1,5 +1,45 @@
 (load "./lib.scm")
 
+(let ()
+  (define obj (let ((o (Object)))
+                (set-obj! o "value" 42)
+                (set-obj! o "getValue" (lambda () this.value))
+                o))
+  (assert (obj.getValue) 42)              ; direct: method auto-bound to obj
+
+  (define getter obj.getValue)
+  (assert (getter) 42)                    ; survives being stored in a var
+
+  (define (fetch o) o.getValue)
+  (assert ((fetch obj)) 42)               ; survives being a function RESULT
+
+  (define arr (vector 3 1 2))
+  (define joiner arr.join)
+  (assert (joiner "-") "3-1-2")           ; native this-dependent method
+
+  (define sorter arr.sort)
+  (sorter (lambda (a b) (- a b)))       ; LIPS lambda crossing into native
+  (assert arr #(1 2 3))
+  (assert (arr.map (lambda (x) (* x x))) #(1 4 9))
+
+  (assert (eq? arr.push.valueOf Function.prototype.valueOf) #t))  ; auto-unbind
+
+(let ()
+  (define foo (lambda (x) (set-obj! this "x" x)))
+  (define bar (new foo 10))
+  (set-obj! foo.prototype 'square (lambda (x) (* x x)))
+  (set-obj! foo.prototype 'sum (lambda (x) (+ this.x x)))
+  (assert (bar.square 10) 100)
+  (assert (bar.sum 5) 15))
+
+(let ()
+  (define foo (lambda (x) (set! this.x x)))
+  (define bar (new foo 10))
+  (set! foo.prototype.square (lambda (x) (* x x)))
+  (set! foo.prototype.sum (lambda (x) (+ this.x x)))
+  (assert (bar.square 10) 100)
+  (assert (bar.sum 5) 15))
+
 (define-syntax syntax-rules-test
   (syntax-rules ()
     ((_ name ...)
