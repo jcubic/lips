d280de2928daa912741924a9f76de6e760b8a8b0
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-09-04 00:34:50 +0200

    add test for binary compiler

--- tests/compile.scm
+++ tests/compile.scm
@@ -4,3 +4,10 @@
         (let* ((serialized (lips.serialize data))
                (deserialized (lips.unserialize serialized)))
           (t.is data deserialized))))
+
+(test "compiler(bin): should create same list structure"
+      (lambda (t)
+        (define data '(foo bar "100" 10 100+20i 10.2 #\x #/foo bar/gi))
+        (let* ((serialized (lips.serialize_bin data))
+               (deserialized (lips.unserialize_bin serialized)))
+          (t.is data deserialized))))
